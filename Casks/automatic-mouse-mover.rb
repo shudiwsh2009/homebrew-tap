73e323f55efc8b8c42da95695c03423923c6b7ba
Shudi Wang <shudiwsh2009@gmail.com>
2023-01-25 09:10:14 +0800

    MOD:[amm] add verified

--- Casks/automatic-mouse-mover.rb
+++ Casks/automatic-mouse-mover.rb
@@ -2,7 +2,8 @@ cask "automatic-mouse-mover" do
     version "1.2.0"
     sha256 "61b509e5c7e2bc7cb4a2db2077cbc0ffb822cd92a4528bf7e9791e2e7bdb97ec"
   
-    url "https://github.com/prashantgupta24/automatic-mouse-mover/releases/download/v#{version}/amm.app.zip"
+    url "https://github.com/prashantgupta24/automatic-mouse-mover/releases/download/v#{version}/amm.app.zip", 
+        verified: "github.com/prashantgupta24/automatic-mouse-mover"
     name "Automatic Mouse Mover"
     desc "a minimalistic go library/app to keep your mac active and alive"
     homepage "https://github.com/prashantgupta24/automatic-mouse-mover"
