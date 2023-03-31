749114ff7e988198a33040a68104a3812e8e2833
Shudi Wang <shudiwsh2009@gmail.com>
2023-03-31 09:07:18 +0800

    ADD: git-credential-manager task

--- Casks/dotnet-sdk6.rb
+++ Casks/dotnet-sdk6.rb
@@ -1,50 +1,50 @@
 cask "dotnet-sdk6" do
-  arch arm: "arm64", intel: "x64"
+	arch arm: "arm64", intel: "x64"
 
-  on_intel do
-    version "6.0.407,91b09d38-854f-4888-ae10-b337b8c42045,3207fd5f87fcc22959311e588e531947"
-    sha256 "a1ca85f4b2b976afb7c053cebf7f29c72a605473ba3df127af8cb2679bc38432"
-  end
-  on_arm do
-    version "6.0.407,e953e1ca-4e28-4e40-b3de-c0bfce0596f1,43ec5885784a56be964baaf67b4a4742"
-    sha256 "c319c9f9bf3de9e17254ba596cc044e807bc5987e8d27f088741221ae441f86e"
-  end
+	on_intel do
+    	version "6.0.407,91b09d38-854f-4888-ae10-b337b8c42045,3207fd5f87fcc22959311e588e531947"
+    	sha256 "a1ca85f4b2b976afb7c053cebf7f29c72a605473ba3df127af8cb2679bc38432"
+  	end
+  	on_arm do
+    	version "6.0.407,e953e1ca-4e28-4e40-b3de-c0bfce0596f1,43ec5885784a56be964baaf67b4a4742"
+    	sha256 "c319c9f9bf3de9e17254ba596cc044e807bc5987e8d27f088741221ae441f86e"
+  	end
 
-  url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
-  name ".NET SDK"
-  desc "Developer platform"
-  homepage "https://www.microsoft.com/net/core#macos"
+  	url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
+  	name ".NET SDK"
+  	desc "Developer platform"
+  	homepage "https://www.microsoft.com/net/core#macos"
 
-  livecheck do
-    cask "dotnet"
-    regex(%r{/download/pr/([^/]+)/([^/]+)/dotnet-sdk-v?(\d+(?:\.\d+)+)-osx-#{arch}\.pkg}i)
-  end
+	livecheck do
+		cask "dotnet"
+		regex(%r{/download/pr/([^/]+)/([^/]+)/dotnet-sdk-v?(\d+(?:\.\d+)+)-osx-#{arch}\.pkg}i)
+	end
 
-  conflicts_with cask: [
-    "dotnet",
-    "dotnet-sdk",
-    "homebrew/cask-versions/dotnet-preview",
-    "homebrew/cask-versions/dotnet-sdk-preview",
-  ], formula: [
-    "dotnet",
-    "dotnet@6",
-  ]
-  depends_on macos: ">= :mojave"
+	conflicts_with cask: [
+		"dotnet",
+		"dotnet-sdk",
+		"homebrew/cask-versions/dotnet-preview",
+		"homebrew/cask-versions/dotnet-sdk-preview",
+	], formula: [
+		"dotnet",
+		"dotnet@6",
+	]
+	depends_on macos: ">= :mojave"
 
-  pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
-  binary "/usr/local/share/dotnet/dotnet"
+	pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
+	binary "/usr/local/share/dotnet/dotnet"
 
-  uninstall pkgutil: [
-              "com.microsoft.dotnet.*",
-              "com.microsoft.netstandard.pack.targeting.*",
-            ],
-            delete:  [
-              "/etc/paths.d/dotnet",
-              "/etc/paths.d/dotnet-cli-tools",
-            ]
+	uninstall	pkgutil: [
+					"com.microsoft.dotnet.*",
+					"com.microsoft.netstandard.pack.targeting.*",
+			  	],
+			  	delete:  [
+					"/etc/paths.d/dotnet",
+					"/etc/paths.d/dotnet-cli-tools",
+				]
 
-  zap trash: [
-    "~/.dotnet",
-    "~/.nuget",
-  ]
+	zap trash: [
+		"~/.dotnet",
+		"~/.nuget",
+	]
 end
