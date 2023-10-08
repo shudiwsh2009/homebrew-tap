52056c9740e8de8a7e19817e6528b01e69acc5c0
Shudi Wang <shudiwsh2009@gmail.com>
2023-10-08 12:50:43 +0800

    FIX: clash-verge missing comma

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -1,7 +1,7 @@
 cask "clash-verge" do
 	arch arm: "aarch64", intel: "x64"
     version "1.3.7"
-    sha256 arm:   "dbf2b5cbb75e833a6e1b6e57c448afc8b03ec60937fcf79766b4697ed7fdf1c2"
+    sha256 arm:   "dbf2b5cbb75e833a6e1b6e57c448afc8b03ec60937fcf79766b4697ed7fdf1c2",
            intel: "f9c63c38fbbcabb385f0a073813577a9c3eba7134926b381c2f129ec40e578ff"
 
 	url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
