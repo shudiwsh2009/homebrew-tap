d1d8bf69990c18cd2b810c4b92bda821286071d9
Shudi Wang <shudiwsh2009@gmail.com>
2023-11-08 12:13:48 +0800

    MOD: bump clash-verge to 1.3.8

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -1,8 +1,8 @@
 cask "clash-verge" do
 	arch arm: "aarch64", intel: "x64"
-    version "1.3.7"
-    sha256 arm:   "dbf2b5cbb75e833a6e1b6e57c448afc8b03ec60937fcf79766b4697ed7fdf1c2",
-           intel: "f9c63c38fbbcabb385f0a073813577a9c3eba7134926b381c2f129ec40e578ff"
+    version "1.3.8"
+    sha256 arm:   "fd8370323a695a8828d1a7487c9e74200ce36b51b324495bfba21498c6fc8c48",
+           intel: "4cb0c3a0abc333af7f3253b9e2700d480128587607e072554c74db4f1ec23df4"
 
 	url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
 	name "Clash Verge"
