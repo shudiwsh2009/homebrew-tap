6c910a34a94ed3634bf10956838fb7799bb069d5
Shudi Wang <shudiwsh2009@gmail.com>
2023-11-17 14:18:52 +0800

    MOD: bump automatic-mouse-mover to 1.3.0

--- Casks/automatic-mouse-mover.rb
+++ Casks/automatic-mouse-mover.rb
@@ -1,8 +1,8 @@
 cask "automatic-mouse-mover" do
-  version "1.2.0"
-  sha256 "61b509e5c7e2bc7cb4a2db2077cbc0ffb822cd92a4528bf7e9791e2e7bdb97ec"
+  version "1.3.0"
+  sha256 "0d152d6cfd114951e21c519b5d5458be7d7bca8dd1207b8990279879fa7b3112"
 
-  url "https://github.com/prashantgupta24/automatic-mouse-mover/releases/download/v#{version}/amm.app.zip", 
+  url "https://github.com/prashantgupta24/automatic-mouse-mover/releases/download/v#{version}/amm.x86.zip", 
     verified: "github.com/prashantgupta24/automatic-mouse-mover"
   name "Automatic Mouse Mover"
   desc "a minimalistic go library/app to keep your mac active and alive"
